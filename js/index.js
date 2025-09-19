c4177e94b8e7fed3d03975ef96f7f40bbc27fcfb
frontenddev344 <frontenddev344@gmail.com>
2025-09-19 10:29:09 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+function openNav() {
+    document.getElementById("mySidenavs").classList.add("open");
+    document.getElementById("menuOverlay").classList.add("active");
+}
+
+function closeNav() {
+    document.getElementById("mySidenavs").classList.remove("open");
+    document.getElementById("menuOverlay").classList.remove("active");
+}
+
+
+//  header sticky js start  
+const header = document.querySelector("header");
+const toggleClass = "is-sticky";
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll > 40) {
+        header.classList.add(toggleClass);
+    } else {
+        header.classList.remove(toggleClass);
+    }
+});
+//  header sticky js end
+
+// Slick Slider Initialization
+$(document).ready(function(){
+    $('.latest-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        fade: false,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>'
+    });
+});
